2e6a72e097aed635667cabd123e8119e54cfcba8
Andy <andy@wisecall.io>
2026-03-26 22:14:14 +0000

    Add legal and professional landing page

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         dental: resolve(__dirname, 'dental.html'),
+        legal: resolve(__dirname, 'legal.html'),
       },
     },
   },
